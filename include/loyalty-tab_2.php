a67f89281014888cc6432912b78135fdb4e45a85
Alexey FromWork <latigovskiy@selvin.local>
2019-12-02 10:42:10 +0300

    add include

--- include/loyalty-tab_2.php
+++ include/loyalty-tab_2.php
@@ -1,51 +1,59 @@
 <div class="two-columns-grid">
     <div class="two-columns-grid__item">
-        <h3>Что мы предлагаем?</h3>
+        <h3>Как получить карту?</h3>
 
-        <p><b>Сеть магазинов КОСМО</b> предлагает своим покупателям участвовать в программе
-            «Постоянный покупатель».
-        </p>
+        <table>
+            <tbody>
+            <tr>
+                <td>
+                    <div class="list-circle-digit">
+                        <div class="list-circle-digit__inner">
+                            1
+                        </div>
+                    </div>
+                </td>
+                <td>Совершить разовую покупку товаров в магазинах КОСМО на сумму от 50 рублей, либо
+                    приобрести карту на кассе по цене 2 рубля</td>
+            </tr>
+            <tr>
+                <td>
+                    <div class="list-circle-digit">
+                        <div class="list-circle-digit__inner">
+                            2
+                        </div>
+                    </div>
+                </td>
+                <td>Заполнить анкету* держателя дисконтной карты КОСМО</td>
+            </tr>
+            <tr>
+                <td>
+                    <div class="list-circle-digit">
+                        <div class="list-circle-digit__inner">
+                            3
+                        </div>
+                    </div>
+                </td>
+                <td>Передать заполненную анкету продавцу</td>
+            </tr>
+            </tbody>
+        </table>
 
-        <p>Для каждого участника программы раскрываются дополнительные возможности:</p>
+        <div class="loyalty__conditions-footer">
+            <div class="loyalty__conditions-fyi">
+                Предоставляемая Вами в анкете информация является конфиденциальной и не подлежит
+                разглашению третьим лицам. * Подписью в анкете владелец дисконтной карты
+                подтверждает,
+                что ознакомлен и согласен с правилами дисконтной программы Космо
+            </div>
 
-        <ul>
-            <li>приобретать товары в <b>магазинах КОСМО</b> со скидкой;</li>
-            <li>участвовать в различных акциях, розыгрышах, которые проводятся
-                среди владельцев дисконтных карт.
-            </li>
-        </ul>
-
-        <div class="loyalty__columns-item-btns">
-            <a href="#popup-card-registration" class="btn fancybox-overflow loyalty__register-btn">Регистрация карты</a>
-
-            <!-- LoyaltySale.loyaltySaleAJAX -->
-            <a href="#popup-check-sale" class="btn btn--transparent fancybox loyalty__check-btn js_check-sale">проверить
-                скидку</a>
+            <a href="#" download class="loyalty__conditions-footer-download btn">Скачать
+                анкету</a>
         </div>
-
-        <p>Мы рады видеть Вас среди постоянных покупателей <b>магазинов КОСМО</b>!</p>
-
-        <a href="https://drive.google.com/file/d/1mNpxXAxGHWhq_yW4_A82O-Z-DNrr0ezT/view"
-           target="_blank"
-           class="loyalty__show-rules-btn clearfix">
-
-            <svg width="38" height="38">
-                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-page"></use>
-            </svg>
-
-            <span>Посмотреть правила «Программы лояльности»</span>
-        </a>
     </div>
 
     <div class="two-columns-grid__item">
-        <div class="loyalty__card">
-            <img src="<?=KOCMO_TEMPLATE_PATH?>/images/loyalty-customer-card.png" alt="">
-        </div>
-
-        <div class="loyalty__subinfo">
-            Более подробную информацию об условиях участия в программе сети КОСМО «Постоянный
-            покупатель» и их скидках можно узнать в соответствующем подразделе или в магазинах
-            Вашего города.
+        <div class="loyalty__condition-bg">
+            <img src="<?=KOCMO_TEMPLATE_PATH?>/images/loyalty-conditions-bg.png" alt="">
         </div>
     </div>
 </div>
